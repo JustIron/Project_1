c35a1a46c5a82242a155d9772fe62f62f688f73f
Iron <Iron@IronPC>
2018-03-03 19:51:38 +0100

    Commit 4

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ and open the template in the editor.
         $znamky[3] = 4;
         
         print_r($znamky);
+        echo("Třetí známka v poli je " . $znamky[2] . ".");
         ?>
     </body>
 </html>
